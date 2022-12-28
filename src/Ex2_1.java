1860acfb34c69a2e924333158e963eb7f8ae4d17
pizone2 <rlawlstj0123@naver.com>
2022-12-28 21:44:22 +0900

    1111

---
+++ src/Ex2_1.java
@@ -0,0 +1,20 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		sysout  ctrl+space 자동완성
+		// ctrl+alt+shift+down 행단위 복사
+		// alt+shift+a 컴럼 편집 모드 (토글)
+		// print() - 출력 후에 줄바꿈을 안함
+		// Println() - 출력 후에 줄바꿈을 합니다.
+		System.out.println(3+5);
+		System.out.println(3-5);	
+		System.out.println(3*5);	
+		System.out.println(3/5);	
+		
+		
+	}
+
+}
+  
